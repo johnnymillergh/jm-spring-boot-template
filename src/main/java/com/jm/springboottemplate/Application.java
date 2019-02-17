2901a9102d3f0ae0bd3e4b73787716360e2e3e7b
Johnny Miller (鍾俊) <johnnysviva@outlook.com>
2019-02-17 17:12:55 +0800

    🆕 Added multi-environment support: development, test and production

--- src/main/java/com/jm/springboottemplate/Application.java
+++ src/main/java/com/jm/springboottemplate/Application.java
@@ -5,12 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * Description: Application, entrance of the project.
+ *
+ * @author: Johnny Miller (鍾俊)
+ * @email: johnnysviva@outlook.com
+ * @date: 2019-02-17
+ * @time: 17:11
+ **/
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.jm.springboottemplate.*.dao")
-public class SpringBootTemplateApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTemplateApplication.class, args);
+        SpringApplication.run(Application.class, args);
         System.out.println("Congratulations! SpringBootTemplate Started!");
     }
 }
