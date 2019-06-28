db4c24405cb8d1e065d9b7c99cdcf6e8642990ea
Johnny Miller (鍾俊) <johnnysviva@outlook.com>
2019-06-28 23:12:08 +0800

    refactor($User): user resultMap to map SQL result

--- jm-spring-boot-starter/src/test/java/com/jmframework/boot/JmSpringBootStarterApplicationTests.java
+++ jm-spring-boot-starter/src/test/java/com/jmframework/boot/JmSpringBootStarterApplicationTests.java
@@ -4,18 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class JmSpringBootStarterApplicationTests {
-
     @Test
     public void contextLoads() {
         String name = StringUtils.trim("  abc abc  ").toLowerCase();
         name = name.replaceAll("\\s", "_");
         log.error("name={}", name);
     }
-
 }
