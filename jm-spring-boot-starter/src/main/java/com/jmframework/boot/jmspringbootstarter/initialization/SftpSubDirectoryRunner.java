1f8868d5aec1abe0423a1aec36471f88a0713cde
Johnny Miller (鍾俊) <johnnysviva@outlook.com>
2019-07-05 09:04:32 +0800

    feat($SFTP): init SFTP server's sub directory
    After Spring container has finished dependency injection, we need an
    ApplicationRunner to init SFTP server's sub directory for our business
    
    BREAKING CHANGE: init SFTP server's sub directory
    
    Closes https://app.clickup.com/t/13g8n3

---
+++ jm-spring-boot-starter/src/main/java/com/jmframework/boot/jmspringbootstarter/initialization/SftpSubDirectoryRunner.java
@@ -0,0 +1,50 @@
+package com.jmframework.boot.jmspringbootstarter.initialization;
+
+import com.jmframework.boot.jmspringbootstarter.configuration.SftpConfiguration;
+import com.jmframework.boot.jmspringbootstarterdomain.common.constant.SftpSubDirectory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.integration.sftp.session.SftpRemoteFileTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * <h1>SftpSubDirectoryRunner</h1>
+ * <p>After dependency injection finished, we must inti the SFTP server's sub directory for out business. If you want
+ * to customize initialization configuration, config SftpSubDirectory.</p>
+ *
+ * @author Johnny Miller (鍾俊), email: johnnysviva@outlook.com
+ * @date 2019-07-05 08:51
+ * @see SftpSubDirectory
+ **/
+@Slf4j
+@Component
+public class SftpSubDirectoryRunner implements ApplicationRunner {
+    private final SftpRemoteFileTemplate sftpRemoteFileTemplate;
+    private final SftpConfiguration sftpConfiguration;
+
+    public SftpSubDirectoryRunner(SftpRemoteFileTemplate sftpRemoteFileTemplate,
+                                  SftpConfiguration sftpConfiguration) {
+        this.sftpRemoteFileTemplate = sftpRemoteFileTemplate;
+        this.sftpConfiguration = sftpConfiguration;
+    }
+
+    @Override
+    @SuppressWarnings("RedundantThrows")
+    public void run(ApplicationArguments args) throws Exception {
+        log.error("Staring to initial SFTP server sub directory.");
+        sftpRemoteFileTemplate.execute(session -> {
+            for (SftpSubDirectory sftpSubDirectory : SftpSubDirectory.values()) {
+                String fullPath = sftpConfiguration.getDirectory() + sftpSubDirectory.getSubDirectory();
+                if (!session.exists(fullPath)) {
+                    log.error("SFTP server sub directory does not exist. Creating sub directory: {}", fullPath);
+                    session.mkdir(fullPath);
+                } else {
+                    log.error("SFTP server sub directory exists. Path: {}", fullPath);
+                }
+            }
+            return null;
+        });
+        log.error("Initialing SFTP server sub directory is done.");
+    }
+}
